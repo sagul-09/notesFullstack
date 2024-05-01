c66056401509ff9a7a50f610e6d1cc482e73c303
sag09 <nihal.evil.royal@gmail.com>
2024-05-01 11:20:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());//this is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
+
+app.use("/", express.static(path.join(__dirname, 'public')));//this is the public folder and the express.static is a buildin middleware function in express to serve static files
+
+app.use('/',require('./routes/root'));//the require function is used to load and cache the root.js file
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')){
+        res.json({message: '404 not found'});
+    }else{
+        res.type('txt').send('404 not found');
+    }});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost://${PORT}`);
+    });
